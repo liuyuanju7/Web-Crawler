78fe515e3bb95b6274aaebc151f87c3618a6ab39
liuyuanju1 <liuyuanju1@jd.com>
2018-08-09 17:33:38 +0800

    创建项目

---
+++ wangyiyun/src/main/java/com/liuyj/WangyiyunApplication.java
@@ -0,0 +1,12 @@
+package com.liuyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangyiyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangyiyunApplication.class, args);
+	}
+}
---
+++ wangyiyun/src/main/java/com/liuyj/entity/Song.java
@@ -0,0 +1,9 @@
+package com.liuyj.entity;
+
+/**
+ * @author liuyuanju1
+ * @date 2018/8/9
+ * @description:
+ */
+public class Song {
+}
